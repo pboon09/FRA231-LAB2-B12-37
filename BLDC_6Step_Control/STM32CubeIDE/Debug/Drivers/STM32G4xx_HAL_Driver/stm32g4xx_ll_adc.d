b502bec2c181e8897805d350a8fa5d9ca873321d
Phakin Boonchanachai <pboonchanachai@gmail.com>
2024-11-16 11:50:15 +0700

    init proj

---
+++ BLDC_6Step_Control/STM32CubeIDE/Debug/Drivers/STM32G4xx_HAL_Driver/stm32g4xx_ll_adc.d
@@ -0,0 +1,2 @@
+Drivers/STM32G4xx_HAL_Driver/stm32g4xx_ll_adc.o: \
+ C:/Users/pboon/OneDrive/Desktop/KMUTT/RMX_Lab/FRA231-LAB2-B12-37/BLDC_6Step_Control/Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_ll_adc.c
